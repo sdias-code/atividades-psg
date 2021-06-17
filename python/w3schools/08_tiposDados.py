86c0048252bb4ca1f9f81d82f2885b79177683a8
sdias-code <silviodias.ms@gmail.com>
2021-06-17 16:33:34 -0400

    Add files data types

---
+++ python/w3schools/08_tiposDados.py
@@ -0,0 +1,29 @@
+# Exercicio 1
+x = 5
+print(type(x))
+
+# Exercicio 2
+x = "Hello World"
+print(type(x))
+
+# Exercicio 3
+x = 20.5
+print(type(x))
+
+# Exercicio 4
+x = ["maça", "banana", "goiaba"]
+print(type(x))
+
+# Exercicio 5
+x = ("maçã", "pera", "uva")
+print(type(x))
+
+# Exercicio 6
+x = {"nome" : "Silvio", "idade": 45}
+print(type(x))
+
+# Exercicio 7
+x = True
+print(type(x))
+
+
