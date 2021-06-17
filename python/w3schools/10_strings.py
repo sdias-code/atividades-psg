6dff58d36301d4c5c1af281a692a4ed390b28b88
sdias-code <silviodias.ms@gmail.com>
2021-06-17 19:23:02 -0400

    Add Python Strings Exercises

---
+++ python/w3schools/10_strings.py
@@ -0,0 +1,35 @@
+# String Exercises
+
+# Exercise 1
+x = "Hello World"
+print(len(x))
+
+# Exercise 2
+txt = "Hello World"
+print(txt[0])
+
+# Exercise 3
+txt = "Hello World"
+print(txt[2 : 5])
+
+# Exercise 4
+txt = " Hello World "
+print(txt.strip())
+
+# Exercise 5
+txt = "Hello World"
+print(txt.upper())
+
+# Exercise 6
+txt = "Hello World"
+print(txt.lower())
+
+# Exercise 7
+txt = "Hello World"
+print(txt.replace("H", "J"))
+
+# Exercise 8
+age = 36
+txt = "My name is Silvio, and I am {}"
+print(txt.format(age))
+
